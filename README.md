b2bf8efbb83cef43e2d61c63bdb798ec8ed959a1
Katch Vidal <katchvidal@gmail.com>
2021-09-02 10:56:02 -0500

    socket basico node

---
+++ README.md
@@ -0,0 +1,5 @@
+# Socket Server Basico
+
+Un servidor de Websockets usando Node, Express y Socket.io
+
+Temas cubiertos en el curso node de 0 -100 de Fernando Herrera 
---
+++ sockets/controller.js
@@ -0,0 +1,28 @@
+const { v4: uuidv4 } = require('uuid');
+
+
+const socketController = (socket) => {
+    
+    console.log('Cliente conectado', socket.id );
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado', socket.id );
+    });
+
+    socket.on('enviar-mensaje', ( payload, callback ) => {
+        
+        const id = uuidv4();
+        callback( id );
+
+        socket.broadcast.emit('enviar-mensaje', payload );
+
+    })
+
+}
+
+
+
+module.exports = {
+    socketController
+}
+
